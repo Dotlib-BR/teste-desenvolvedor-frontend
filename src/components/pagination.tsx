70bb980ea3ef20d0a13e72a313e2664a8eb80916
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-04-16 15:51:15 -0300

    feat: add Pagination component

---
+++ src/components/pagination.tsx
@@ -0,0 +1,31 @@
+// Components
+import { Button } from "./ui/button"
+
+interface PaginationProps {
+  dataLength: number
+  pageNum: number
+  onNextClick: () => void
+  onPrevClick: () => void
+}
+
+export const Pagination = ({
+  dataLength,
+  pageNum,
+  onNextClick,
+  onPrevClick,
+}: PaginationProps) => {
+  const canGoToNext = dataLength / 10 > pageNum
+  const canGoToPrev = pageNum !== 1
+
+  return (
+    <div className="flex items-center justify-center gap-4 ">
+      <Button onClick={onPrevClick} disabled={!canGoToPrev}>
+        Anterior
+      </Button>
+      <div>{pageNum}</div>
+      <Button onClick={onNextClick} disabled={!canGoToNext}>
+        Próximo
+      </Button>
+    </div>
+  )
+}
