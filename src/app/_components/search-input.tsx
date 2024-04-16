195f63931b559b54e2168a94ee8638d20203c2d5
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-16 09:58:16 -0300

    Add SearchInput component

---
+++ src/app/_components/search-input.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { MutableRefObject, useEffect, useRef } from "react";
+
+/** Properties for the {@link SearchInput} component. */
+interface SearchInputProperties {
+  /** Current value of the input. */
+  value?: string | undefined;
+  /**
+   * Ref that receives the current value of the input. This can be used to avoid
+   * re-rendering components when the value changes.
+   */
+  valueRef?: MutableRefObject<string> | undefined;
+  /** Listener for the input `change` event. */
+  onChange?: ((event: React.ChangeEvent<HTMLInputElement>) => void) | undefined;
+  /** Placeholder for the input. */
+  placeholder: string;
+}
+
+/**
+ * A search input.
+ * @param properties - Properties for the component.
+ */
+export function SearchInput(properties: SearchInputProperties) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current != null && properties.valueRef != null)
+      properties.valueRef.current = inputRef.current.value;
+  }, [properties.value]);
+
+  return (
+    <input
+      ref={inputRef}
+      type="text"
+      value={properties.value}
+      placeholder={properties.placeholder}
+      onChange={(event) => {
+        if (properties.valueRef != null)
+          properties.valueRef.current = event.target.value;
+        properties.onChange?.(event);
+      }}
+      className="rounded-lg bg-gray-200 px-4 py-2"
+    ></input>
+  );
+}
+
+export default SearchInput;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
-import Heading from "./_components/heading";
+"use client";
+
+import { useRef } from "react";
+import SearchInput from "./_components/search-input";
 import Section from "./_components/section";
 
 export default function Home() {
+  const searchRef = useRef<string>("");
+
   return (
     <main className="flex min-h-screen items-center justify-center text-lg">
-      <Section title="Remed.io" className="flex flex-col items-center gap-8">
-        Pesquise medicações aqui
+      <Section
+        title="Remed.io"
+        className="flex flex-col items-center gap-8"
+        contentClassName="flex flex-col items-center gap-4"
+      >
+        <label className="contents">
+          Pesquise medicações aqui
+          <SearchInput
+            placeholder="Nome da medicação"
+            valueRef={searchRef}
+          ></SearchInput>
+        </label>
       </Section>
     </main>
   );
