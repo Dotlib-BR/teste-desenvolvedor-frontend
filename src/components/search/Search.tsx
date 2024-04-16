40e5f2a63fd46019b8371f81fadb2c4f953588a1
Gardiano <joaopaulong@hotmail.com>
2024-04-16 15:09:27 -0300

    implement Search component functions

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,11 +1,40 @@
 
+import { ChangeEvent, useEffect, useState } from 'react';
 import './Search.scss';
 
+import { getDataByNameOrLab } from '../../services/axios';
+import { ToastContainer } from 'react-toastify';
+
 export const Search = () => {
+
+  const [value, setValue] = useState<string>('');
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setValue(value);
+    getDataByNameOrLab(value);
+  };
+
   return (
-    <main>
-      <h1> Procure fármacos ou laboratórios que atendam sua necessidade. </h1>
-      <input type="input" placeholder='  Ex: amoxicilina ou medlabs' />
-    </main>
-  )
+    <>
+      <main>
+        <h1> Procure fármacos ou laboratórios que atendam sua necessidade. </h1>
+        <input
+          type="input"
+          value={value}
+          onChange={handleChange}
+          placeholder='  Ex: amoxicilina ou medlabs' />
+      </main>
+      <ToastContainer 
+          position="bottom-right"
+          closeOnClick
+          limit={ 2 }
+          className='toastSend'
+      />
+    </>
+  );
 }
