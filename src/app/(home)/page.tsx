e450d088be043e584846155b43580d32766678f7
Paulo Lima <plima12121984@gmail.com>
2024-04-16 15:03:00 -0300

    chore: add lib dayjs on project

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,41 @@
 import { useHome } from './useHome'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export default async function Home() {
   const { medicines } = await useHome()
 
   return (
-    <main>
+    <main className='flex flex-col  items-center justify-center py-6'>
       <h1>Dotlib Farma</h1>
-      {medicines.map((medicine) => {
-        return (
-          <ul key={medicine.id}>
-            <a target='_blank' href='/pdf_sample.pdf'>
-              <li>{medicine.name}</li>
-            </a>
-          </ul>
-        )
-      })}
+      <div className='flex flex-wrap w-[1280px]'>
+        {medicines.map((medicine) => {
+          return (
+            <Card key={medicine.id}>
+              <CardHeader>
+                <CardTitle>
+                  {medicine.name.substring(1, 15).concat('...')}
+                </CardTitle>
+                <CardDescription>{medicine.company}</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <strong>Data de publicação:</strong>
+                <span>{medicine.published_at}</span>
+              </CardContent>
+              <CardFooter>
+                <p>Card Footer</p>
+              </CardFooter>
+            </Card>
+          )
+        })}
+      </div>
     </main>
   )
 }
