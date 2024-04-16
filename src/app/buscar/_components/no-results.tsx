1c08ef138dd7501d297697458689798cd31c90c9
Luiz Fernando F. G. Valle <luizfvalle@pm.me>
2024-04-16 12:02:49 -0300

    Use history to go back

--- src/app/buscar/_components/no-results.tsx
+++ src/app/buscar/_components/no-results.tsx
@@ -2,7 +2,6 @@
 
 import Button from "@/app/_components/button";
 import { ArrowLeftIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
 
 /** Properties for the {@link NoResults} component. */
 export interface NoResultsProperties {
@@ -11,12 +10,10 @@ export interface NoResultsProperties {
 
 /** Component to display when there are no search results. */
 export default function NoResults(properties: NoResultsProperties) {
-  const router = useRouter();
-
   return (
     <div className="flex flex-col items-center gap-4">
       <div>{properties.message ?? "Nenhum resultado"}</div>
-      <Button onClick={() => router.push("/")}>
+      <Button onClick={() => history.back()}>
         <ArrowLeftIcon />
         Voltar
       </Button>
