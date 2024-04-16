2130ebab5c9b25dd1a965006060fa3f7471fd02f
Fernando Bonatto <febonatto_1999@hotmail.com>
2024-04-16 14:39:39 -0300

    chore: Adicionado alguns componentes e configurações do shadcn/ui e configurado components.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
     },
     extend: {
       colors: {
+        dotlib: 'hsl(var(--dotlib))',
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
