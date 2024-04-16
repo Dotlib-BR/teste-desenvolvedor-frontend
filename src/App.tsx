22e495113642590f286372d29d1e928014b353d7
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-04-16 11:26:24 -0300

    chore: add shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 const App = () => {
-  return <div className="bg-white text-black">...</div>
+  return (
+    <div className="bg-white text-black">
+      <Button>Teste</Button>
+    </div>
+  )
 }
 
 export default App
