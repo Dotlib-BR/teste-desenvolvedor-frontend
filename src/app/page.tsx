4804fd8cb0c33067a8dc716baa27943405174af9
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-04-17 17:37:44 -0300

    [feat]: Adicionado header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
-import { List } from "@/components/List/List"
+import { Header } from '@/components/Header/Header'
+import { List } from '@/components/List/List'
 
 export default async function Home() {
 	const teste = await (await fetch('http://localhost:3000/data/')).json()
 	console.log(teste)
 
 	return (
-		<main>
-			<List />
-		</main>
+		<>
+			<Header />
+			<main>
+				<List />
+			</main>
+		</>
 	)
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import headerStyle from './HeaderStyle.module.scss'
+
+export const Header = () => {
+	return (
+		<div className={headerStyle.header}>
+			<h1 >Bem vindo ao bulário eletrônico</h1>
+		</div>
+	)
+}
