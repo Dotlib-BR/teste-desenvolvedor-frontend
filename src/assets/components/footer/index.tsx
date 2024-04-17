622658648403d4af3c503ec17ceadbc37b647576
12Gustavo21 <gustavo927318@gmail.com>
2024-04-17 18:10:40 -0300

    style: fix style do medicineList feat: implementing footer

--- src/assets/components/footer/index.tsx
+++ src/assets/components/footer/index.tsx
@@ -1,5 +1,23 @@
+import "./style.sass";
+
 export default function index() {
   return (
-    <div>footer</div>
-  )
+    <>
+      <footer>
+        <section className="footer__container">
+          <h3>Teste para vaga de desenvolvedor front-end</h3>
+          <p>
+            Desenvolvido por{" "}
+            <a
+              href="https://github.com/12Gustavo21"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Gustavo Almeida
+            </a>
+          </p>
+        </section>
+      </footer>
+    </>
+  );
 }
