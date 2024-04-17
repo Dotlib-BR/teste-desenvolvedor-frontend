a034673dd3523fbef7af6027fb506e43bff46a41
Dev Luiz Alves <109562299+LuizDevExe@users.noreply.github.com>
2024-04-17 16:31:13 -0300

    Criação e estilização do componente Header

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css"
+import MainLogo from "../../Assets/MedicalLogo.png"
+
+function Header({MainHeaderText}) {
+  return (
+    <header>
+        <h1>
+            {MainHeaderText}
+        </h1>
+        <img src={MainLogo} alt="ìcone representando uma lupa e um bastão de esculápio, logo principal da marca"/>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Pages/Main/Main.jsx
@@ -0,0 +1,16 @@
+import './Main.css'
+import Header from '../../Components/Header/Header'
+function Main() {
+  return (
+    <>
+      <Header
+        MainHeaderText={"Bulário Eletrônico"}
+      />
+      <main>
+        
+      </main>
+    </>
+  )
+}
+
+export default Main
