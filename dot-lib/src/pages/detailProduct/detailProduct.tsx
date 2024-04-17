504d8292fb3981a679e48b3d4366db6d5d643623
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-16 21:56:47 -0300

    feat: add details product

--- dot-lib/src/pages/detailProduct/detailProduct.tsx
+++ dot-lib/src/pages/detailProduct/detailProduct.tsx
@@ -1,3 +1,66 @@
+import styles from './styles.module.scss'
+import { formatDate } from '../../utils'
+import { useDetailProduct } from './useDetailProduct'
+import { Button } from '../../components/ui'
+
 export function DetailProduct() {
-  return <main>DetailProduct</main>
+  const { product, handleOpenDoc } = useDetailProduct()
+
+  if (!product) return <></>
+  return (
+    <main className={styles.detailProduct}>
+      <p>
+        <strong>Produto: </strong> {product?.name}
+      </p>
+      <p>
+        <strong>Laboratório: </strong> {product?.company}
+      </p>
+      <p>
+        <strong>Publicado em: </strong>{' '}
+        {formatDate({ date: product?.published_at, hours: true })}
+      </p>
+      <div className={styles.priciples}>
+        <strong>Contém:</strong>
+        {product.active_principles.map((principles, index) => (
+          <p key={principles.id}>
+            {principles.name}
+            {index + 1 < product.active_principles.length && ','}
+          </p>
+        ))}
+      </div>
+      <div className={styles.documents}>
+        <strong>Documentos: </strong>
+        <div className={styles.allDocument}>
+          {product.documents.map((doc) => (
+            <div key={doc.id} className={styles.document}>
+              <p>
+                <strong>Registro da bula em orgãos responsáveis:</strong>{' '}
+                {doc.expedient}
+              </p>
+              <div className={styles.bula}>
+                <p>
+                  <strong>
+                    {doc.type === 'PATIENT'
+                      ? 'Bula para pacientes'
+                      : 'Bula para profissionais de saúde'}
+                    :
+                  </strong>
+                </p>
+                <div className={styles.buttonsArea}>
+                  <a href={doc.url} download="bula.pdf">
+                    <Button iconLeft="download" />
+                  </a>
+
+                  <Button
+                    iconLeft="see"
+                    onClick={() => handleOpenDoc(doc.url)}
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  )
 }
---
+++ dot-lib/src/pages/detailProduct/useDetailProduct.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { api } from '../../services/api'
+import { ProductProps } from '../../contexts/productContext'
+
+export function useDetailProduct() {
+  const { id } = useParams()
+  const [product, setProduct] = useState<ProductProps | null>(null)
+
+  useEffect(() => {
+    async function getProduct() {
+      try {
+        const response = await api.get(`/data/${id}`)
+
+        console.log(response.data)
+        setProduct(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getProduct()
+  }, [])
+
+  const handleOpenDoc = (url: string) => {
+    window.open(url, '_blank')
+  }
+  return { product, handleOpenDoc }
+}
