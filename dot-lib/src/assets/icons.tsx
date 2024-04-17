bf1a4bb82ed4ab2c939c73d111f96528f1caa1c4
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-16 21:55:22 -0300

    feat: add new icon

--- dot-lib/src/assets/icons.tsx
+++ dot-lib/src/assets/icons.tsx
@@ -1,7 +1,8 @@
-import { FaSearch, FaFileDownload, FaFilePdf } from 'react-icons/fa'
+import { FaSearch, FaFileDownload, FaFilePdf, FaEye } from 'react-icons/fa'
 
 export const icons = {
   search: <FaSearch />,
   download: <FaFileDownload />,
   pdf: <FaFilePdf />,
+  see: <FaEye />,
 }
