d322b203c1255b6da26959e296438706f0e60029
Gardiano <joaopaulong@hotmail.com>
2024-04-17 12:53:31 -0300

    added buttons to pagination

--- src/components/medicine/Medicine.tsx
+++ src/components/medicine/Medicine.tsx
@@ -2,12 +2,26 @@
 import { useEffect, useContext } from "react";
 import { DataContext } from "../../context/DataContext";
 import { Card } from "../Card/Card";
-import './Medicine.scss';
+
 import { Loaders } from "../../helpers/Loaders";
 import { Cards } from "../../model/card";
 
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+
+import './Medicine.scss';
+
 export const Medicine = () => {
-  const { data, getData, searched_data } = useContext(DataContext);
+  const {
+    data,
+    getData,
+    prevPage,
+    nextPage,
+    currentPage,
+    searched_data,
+    indexOfLastItem,
+    dataForPagination
+  } = useContext(DataContext);
 
   useEffect(() => {
     getData();
@@ -16,25 +30,34 @@ export const Medicine = () => {
   return (
     <>
       {searched_data || data ? (
-        <section>
-          {(!searched_data ? data : searched_data)?.map((data: Cards) => (
-            <Card
-              key={data.id}
-              id={data.id}
-              name={data.name}
-              company={data.company}
-              published_at={data.published_at}
-            />
-          ))}
-        </section>
+        <>
+          <section>
+            {(!searched_data ? dataForPagination : searched_data)?.map((data: Cards) => (
+              <Card
+                key={data.id}
+                id={data.id}
+                name={data.name}
+                company={data.company}
+                published_at={data.published_at}
+              />
+            ))}
+          </section>
+          <div className="pagination">
+            <button
+              onClick={prevPage}
+              disabled={currentPage === 1}>
+              <IoIosArrowBack />
+            </button>
+            <button
+              onClick={nextPage}
+              disabled={!data || indexOfLastItem >= data.length}>
+              <IoIosArrowForward />
+            </button>
+          </div>
+        </>
       ) : (
         <Loaders />
       )}
     </>
-  );
-  
-
-  
-  
-
+  )
 }
