5d9217d7d58991bb04e53d3164eca84faaf3b6ce
Pablo Alves <ribeirojr1522@gmail.com>
2024-04-16 22:00:27 -0300

    feat: add link initial page in header

--- dot-lib/src/components/layout/modules/header/header.tsx
+++ dot-lib/src/components/layout/modules/header/header.tsx
@@ -1,11 +1,20 @@
 import styles from './styles.module.scss'
 import Logo from '../../../../assets/logo.png'
+import { Link, useLocation } from 'react-router-dom'
+import { Button } from '../../../ui'
 
 export function Header() {
+  const { pathname } = useLocation()
+
   return (
     <header className={styles.header}>
       <div className={`container ${styles.content}`}>
         <img src={Logo} alt="Logo Dot.lib" className={styles.logo} />
+        {pathname !== '/' && (
+          <Link to={'/'}>
+            <Button>Página inicial</Button>
+          </Link>
+        )}
       </div>
     </header>
   )
