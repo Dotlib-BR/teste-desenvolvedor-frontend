63e7f91f2bfe30fd6156a617048b87f4ea16af97
NatanaelJose <josesaraivamaciel@gmail.com>
2024-04-17 16:04:42 -0300

    [Setup]: Add initial website files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <header className="bg-blue-500 p-4">
+        <h1 className="text-white text-2xl font-semibold">Bulário Eletrônico</h1>
+      </header>
+      <main className="container mx-auto p-4">
+        <p className="bg-red-500">Olá</p>
+      </main>
+      <footer className="bg-blue-500 p-4 text-white text-center">
+        &copy; 2024 Bulário Eletrônico. Todos os direitos reservados.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
