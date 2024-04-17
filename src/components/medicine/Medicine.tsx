d5def97b0591e35fb5241f801cdfc4cecff223e0
Gardiano <joaopaulong@hotmail.com>
2024-04-17 11:00:30 -0300

    add cards component to medicine component

--- src/components/medicine/Medicine.tsx
+++ src/components/medicine/Medicine.tsx
@@ -1,12 +1,40 @@
-import { useEffect } from "react";
-import { getData } from "../../services/axios";
+
+import { useEffect, useContext } from "react";
+import { DataContext } from "../../context/DataContext";
+import { Card } from "../Card/Card";
+import './Medicine.scss';
+import { Loaders } from "../../helpers/Loaders";
+import { Cards } from "../../model/card";
 
 export const Medicine = () => {
+  const { data, getData, searched_data } = useContext(DataContext);
+
   useEffect(() => {
     getData();
   }, []);
 
   return (
-    <>medicine;</>
-  )
+    <>
+      {searched_data || data ? (
+        <section>
+          {(!searched_data ? data : searched_data)?.map((data: Cards) => (
+            <Card
+              key={data.id}
+              id={data.id}
+              name={data.name}
+              company={data.company}
+              published_at={data.published_at}
+            />
+          ))}
+        </section>
+      ) : (
+        <Loaders />
+      )}
+    </>
+  );
+  
+
+  
+  
+
 }
