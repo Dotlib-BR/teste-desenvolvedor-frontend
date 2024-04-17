8b75c0b790b2c520db6c15c0aaa3cb01fd2f943d
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-17 12:55:00 -0300

    feat:: add content in header

--- bulatorio-eletronico/src/components/header/index.tsx
+++ bulatorio-eletronico/src/components/header/index.tsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo.svg'
+
+import s from './style.module.sass'
+
 export default function Header() {
-    return <header>header</header>
+    return (
+        <header className={s.header}>
+            <figure>
+                <Link to={'/'}>
+                    <img src={Logo} className={s.logo} alt='Logo dot.lib' />
+                </Link>
+            </figure>
+        </header>
+    )
 }
