e14cf767412d7d3b393cb7b0572afa76c6b9d8c9
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-17 16:23:52 -0300

    Add Header component

---
+++ src/ui/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import "./Header.scss";
+
+export const Header = () => {
+  return (
+    <header className="header">
+      <img
+        className="header-logo"
+        src="Leaflet-Collection-Logo.svg"
+        alt="Leaflet Collection Logo"
+        title="Leaflet Collection Logo"
+      />
+    </header>
+  );
+};
