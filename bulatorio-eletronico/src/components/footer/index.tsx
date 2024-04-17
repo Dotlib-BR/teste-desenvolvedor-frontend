fb721ec8afe0ba79d1ca54b485bb888d4c2ca46d
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-17 13:53:44 -0300

    feat: add content in footer

--- bulatorio-eletronico/src/components/footer/index.tsx
+++ bulatorio-eletronico/src/components/footer/index.tsx
@@ -1,3 +1,12 @@
+import s from './style.module.sass'
+
 export default function Footer() {
-    return <footer>footer</footer>
+    return (
+        <footer className={s.footer}>
+            <p>
+                Desenvolvido com ❤️ por{' '}
+                <a href='https://www.brunobarreiras.com.br'>Bruno Barreiras</a>
+            </p>
+        </footer>
+    )
 }
