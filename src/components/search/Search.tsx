5a01082743db38a5780ad0adcde7fe08431131f5
Gardiano <joaopaulong@hotmail.com>
2024-04-17 13:09:29 -0300

    code changes

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,17 +1,13 @@
 
-import { ChangeEvent, useEffect, useState } from 'react';
+import { ChangeEvent, useState, useContext } from 'react';
 import './Search.scss';
 
-import { getDataByNameOrLab } from '../../services/axios';
 import { ToastContainer } from 'react-toastify';
+import { DataContext } from '../../context/DataContext';
 
 export const Search = () => {
-
+  const { getDataByNameOrLab } = useContext(DataContext);
   const [value, setValue] = useState<string>('');
-
-  useEffect(() => {
-
-  }, []);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -22,7 +18,7 @@ export const Search = () => {
   return (
     <>
       <main>
-        <h1> Procure fármacos ou laboratórios que atendam sua necessidade. </h1>
+        <h1> Encontre fármacos ou laboratórios que atendam sua necessidade. </h1>
         <input
           type="input"
           value={value}
@@ -30,10 +26,10 @@ export const Search = () => {
           placeholder='  Ex: amoxicilina ou medlabs' />
       </main>
       <ToastContainer 
-          position="bottom-right"
-          closeOnClick
-          limit={ 2 }
-          className='toastSend'
+        position="bottom-right"
+        closeOnClick
+        limit={ 1 }
+        className='toastSend'
       />
     </>
   );
