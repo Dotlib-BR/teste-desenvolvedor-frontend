4a43b3ba68e6ed982f3653f42cf2997ed4e0b468
Gardiano <joaopaulong@hotmail.com>
2024-04-18 10:21:52 -0300

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@
 import './App.scss';
 import { AppRoutes } from './Routes';
 import { Header } from './components/Header/Header';
+import { DataProvider } from './context/DataContext';
 
 function App() {
   return (
-      <>
-        <Header />
+    <>
+      <Header />
+      <DataProvider>
         <AppRoutes />
-      </>
+      </DataProvider>
+    </>
   )
 }
-
 export default App
