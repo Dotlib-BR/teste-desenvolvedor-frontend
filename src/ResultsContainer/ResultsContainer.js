cd93a19aa42bcb884addc6c5c5de7c808c630e9f
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2024-04-17 23:24:50 -0300

    feat: criado componente 'ResultsContainer'.

---
+++ src/ResultsContainer/ResultsContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class SearchField extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+        };
+    }
+
+    render() {
+
+      return (
+        <div >
+        </div>
+      );
+    }
+}
