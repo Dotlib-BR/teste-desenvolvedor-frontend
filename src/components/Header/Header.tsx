2415f2c221e99c7e3da947d11c1b37dddca9bae7
edilan-ribeiro <eddyoneal2@hotmail.com>
2024-04-17 21:44:35 -0300

    [feat]: Adicionados estilos de erro, input e header recebeu um link

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,9 @@ import headerStyle from './HeaderStyle.module.scss'
 export const Header = () => {
 	return (
 		<div className={headerStyle.header}>
-			<h1 >Bem vindo ao bulário eletrônico</h1>
+			<a href='/'>
+				Bem vindo ao bulário eletrônico
+			</a>
 		</div>
 	)
 }
