869c9e04e6e66e2c1aac20e102b0a60bf43b2cc1
Gardiano <joaopaulong@hotmail.com>
2024-04-18 10:22:45 -0300

    details routes to Details component added

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,16 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Home } from './pages/Home';
+import { Details} from './components/MedicineDetails/Details'
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={ <Home/> } />
+      </Routes>
+      <Routes>
+        <Route path="/data/:id" element={ <Details /> } />
       </Routes>
     </BrowserRouter>
   );
