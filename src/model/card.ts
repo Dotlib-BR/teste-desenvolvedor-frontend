8bcef7e92122ef6ce456be5ded1aa0bfe52a4ff8
Gardiano <joaopaulong@hotmail.com>
2024-04-18 10:19:30 -0300

    more type data added

--- src/model/card.ts
+++ src/model/card.ts
@@ -1,9 +1,14 @@
 
 export type Cards = {
   id: string
-  name: string
-  company: string
+  name: string 
+  company: string 
   published_at: string
+  expedient: string
+  type: string
+  url: string
+  active_principles?: []
+  documents?: []
   length?: any
   map?: any
   slice?: any
