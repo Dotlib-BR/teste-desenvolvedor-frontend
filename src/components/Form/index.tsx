477cd8549cdd9955c3cf03e0ba43ddcd9954c814
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2024-04-18 00:29:43 -0300

    feat: create form component

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { FormHTMLAttributes } from 'react'
 
-export const Form: React.FC = () => {
+import { FormProvider, useForm, SubmitHandler } from 'react-hook-form'
+import { ZodSchema } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+type FormData<T extends ZodSchema> =
+  T extends ZodSchema<infer Data> ? Data : never
+
+interface FormProps<T extends ZodSchema>
+  extends FormHTMLAttributes<HTMLFormElement> {
+  children: React.ReactNode
+  formSchema: T
+  onSubmitForm: SubmitHandler<FormData<T>>
+  className?: string
+}
+
+export const Form = <T extends ZodSchema>({
+  children,
+  formSchema,
+  onSubmitForm,
+  className,
+}: FormProps<T>) => {
+  const methods = useForm<FormData<T>>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
+    event,
+  ) => {
+    methods.handleSubmit(async (data) => {
+      await onSubmitForm(data)
+    })(event)
+  }
+
   return (
-    <div>
-      <span>Form</span>
-    </div>
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit} className={className}>
+        {children}
+      </form>
+    </FormProvider>
   )
 }
