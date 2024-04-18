69444c9310d87bfe90c325756af96a941b040b28
Gguife <gguife747@gmail.com>
2024-04-18 18:55:07 -0300

    feat: Adicionando rota dinamica para medicamentos

--- frontend/src/routes/MainRoutes.tsx
+++ frontend/src/routes/MainRoutes.tsx
@@ -4,13 +4,15 @@ import { Product } from "../types";
 //Views
 import ProductListPage from "../views/productsListPage";
 import FormProductPage from "../views/formSearchProductPage";
+import ProductPage from "../views/productContentPage";
 
 export const MainRouter = () =>{
   const [ productsList, setProductsList ] = useState<Product[]>([]);
 
   const router = useRoutes([
     {path: "/", element: <FormProductPage setProductsList={setProductsList}/>},
-    {path: "/medicamentos", element: <ProductListPage productList={productsList} />}
+    {path: "/medicamentos", element: <ProductListPage productList={productsList} />},
+    {path: "/medicamento/:id", element: <ProductPage />}
   ])
 
   return router;
