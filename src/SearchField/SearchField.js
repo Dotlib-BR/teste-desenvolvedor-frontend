7b624cb87f5df72015276a0ae511f91bc1225e0a
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2024-04-17 22:16:09 -0300

    criado componente 'SearchField'

---
+++ src/SearchField/SearchField.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SearchField.css';
+
+
+export default class SearchField extends React.Component {
+
+    render() {
+      return (
+        <>
+        </>
+      );
+    }
+}
