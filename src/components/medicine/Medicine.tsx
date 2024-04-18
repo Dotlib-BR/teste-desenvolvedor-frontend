f7794069b70861e883735d85e1aa8aa71ed95e65
Gardiano <joaopaulong@hotmail.com>
2024-04-18 14:04:26 -0300

    refactoring code component and logic

--- src/components/medicine/Medicine.tsx
+++ src/components/medicine/Medicine.tsx
@@ -1,15 +1,10 @@
-
 import { useEffect, useContext } from "react";
 import { DataContext } from "../../context/DataContext";
 import { Card } from "../Card/Card";
-
 import { Loaders } from "../../helpers/Loaders";
-import { Cards } from "../../model/card";
-
-import { IoIosArrowForward } from "react-icons/io";
-import { IoIosArrowBack } from "react-icons/io";
-
 import './Medicine.scss';
+import { Cards } from "../../model/card";
+import Pagination from "../Pagination/Pagination";
 
 export const Medicine = () => {
   const {
@@ -20,43 +15,51 @@ export const Medicine = () => {
     currentPage,
     searched_data,
     indexOfLastItem,
-    dataForPagination
+    dataForPagination,
+    searched_value,
   } = useContext(DataContext);
 
   useEffect(() => {
     getData();
   }, []);
 
+  const renderCards = (cards?: Cards) => (
+    <section>
+      {cards?.map((data: Cards) => (
+        <Card
+          key={data.id}
+          id={data.id}
+          name={data.name}
+          company={data.company}
+          published_at={data.published_at}
+        />
+      ))}
+    </section>
+  );
+
   return (
     <>
-      {searched_data || data ? (
+      {searched_value?.length > 2 ? (
         <>
-          <section>
-            {(!searched_data ? dataForPagination : searched_data)?.map((data: Cards) => (
-              <Card
-                key={data.id}
-                id={data.id}
-                name={data.name}
-                company={data.company}
-                published_at={data.published_at}
-              />
-            ))}
-          </section>
-          <div className="pagination">
-            <button
-              onClick={prevPage}
-              disabled={currentPage === 1}>
-              <IoIosArrowBack />
-            </button>
-            <button
-              onClick={nextPage}
-              disabled={!data || indexOfLastItem >= data.length}>
-              <IoIosArrowForward />
-            </button>
-          </div>
+          {searched_data && searched_data.length === 0 ? <b>informações não encontradas.</b> : renderCards(searched_data)}
         </>
       ) : (
-        <Loaders />
+        <>
+          {dataForPagination ? (
+            <>
+              {renderCards(dataForPagination)}
+              <Pagination
+                currentPage={currentPage}
+                prevPage={prevPage}
+                nextPage={nextPage}
+                disabledPrev={currentPage === 1}
+                disabledNext={!data || indexOfLastItem >= data.length}
+              />
+            </>
+          ) : (
+            <Loaders />
+          )}
+        </>
       )}
     </>
   )
