405b4cbc8796bc7a9dad2d54b9ed3bb8834a8bee
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-18 14:42:56 -0300

    Add Card component

---
+++ src/ui/components/Card/Card.tsx
@@ -0,0 +1,58 @@
+import { ResponseLeafletMapper } from "../../../service/types";
+import { RiStarLine } from "react-icons/ri";
+import "./Card.scss";
+
+interface CardProps {
+  item: ResponseLeafletMapper;
+}
+
+export const Card = ({ item }: CardProps) => {
+  return (
+    <article className="card-container">
+      <header className="card-header">
+        <span className="card-title">Nome:</span>
+        <h2>
+          {item.name.map((item, index) => (
+            <span key={index} className="card-name">
+              {index === 0 ? item : `+ ${item}`}
+            </span>
+          ))}
+        </h2>
+      </header>
+      <div className="medicine-info">
+        <div className="info-container">
+          <span className="card-title">Fabricante:</span>
+          <span>{item.company}</span>
+        </div>
+        <div className="info-container">
+          <span className="card-title">Data:</span>
+          <span>{item.published_at}</span>
+        </div>
+        <div className="info-container">
+          <span className="card-title">Substâncias Ativas:</span>
+          {item.active_principles.map((item) => (
+            <span key={item.id}>{item.name}</span>
+          ))}
+        </div>
+      </div>
+      <div className="documents-info">
+        <span className="card-title">Documentos</span>
+        {item.documents.map((item) => (
+          <div key={item.id}>
+            <div className="info-container">
+              <span className="card-title">Expediente:</span>
+              <span>{item.expedient}</span>
+            </div>
+            <div className="info-container">
+              <span className="card-title">Tipo:</span>
+              <span>{item.type}</span>
+            </div>
+          </div>
+        ))}
+        <div className="favorite-container">
+          <RiStarLine />
+        </div>
+      </div>
+    </article>
+  );
+};
--- src/ui/components/index.ts
+++ src/ui/components/index.ts
@@ -1,5 +1,6 @@
 export { App } from "./App";
 export { Button } from "./Button";
+export { Card } from "./Card";
 export { Header } from "./Header";
 export { Input } from "./Input";
 export { Layout } from "./Layout";
