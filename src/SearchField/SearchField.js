ca7fe0f8df39a8160b9caa3c6861a82ea7784d23
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2024-04-17 23:00:20 -0300

    feat: componente 'SearchField estilizado

--- src/SearchField/SearchField.js
+++ src/SearchField/SearchField.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import './SearchField.css';
+import style from './SearchField.css';
 
 
 export default class SearchField extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const data = {
+            text: formData.get('text'),
+            select: formData.get('select')
+        };
+
+
+        window.alert(data);
+    }
+
     render() {
+
       return (
-        <>
-        </>
+
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" name="text" required placeholder="Medicamento ou Laboratório..." />
+                <select name="select" id="target">
+                    <option value="medicamento">Medicamento</option>
+                    <option value="laboratorio">Laboratório</option>
+                </select>
+                <button type="submit">Buscar</button>
+            </form>
+
+        </div>
       );
     }
 }
