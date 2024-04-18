41fe3354fe522a8ff8196111cf50e50533ddb637
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-18 16:45:44 -0300

    feat: add search

--- electronic-booklet/src/pages/home/components/hero/index.tsx
+++ electronic-booklet/src/pages/home/components/hero/index.tsx
@@ -1,15 +1,39 @@
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import SearchImg from '../../../../assets/search.svg'
 import s from './style.module.sass'
 
 export default function Hero() {
+    const [searchValue, setSearchValue] = useState<string>('')
+    const navigate = useNavigate()
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        navigate(`/query?search=${searchValue}`)
+    }
+
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        const value = e.target.value.toUpperCase()
+        setSearchValue(value)
+    }
+
     return (
         <section className={s.containerHero}>
-            <input
-                type='text'
-                name='search'
-                id='search'
-                placeholder='Digite o nome do medicamento'
-                className={s.inputSearch}
-            />
+            <form className={s.formSearch} onSubmit={handleSubmit}>
+                <input
+                    type='text'
+                    name='search'
+                    id='search'
+                    placeholder='Digite o nome do medicamento'
+                    className={s.inputSearch}
+                    value={searchValue}
+                    onChange={handleChange}
+                    required
+                />
+                <button type='submit' className={s.searchBtn}>
+                    <img src={SearchImg} alt='search icon' />
+                </button>
+            </form>
         </section>
     )
 }
