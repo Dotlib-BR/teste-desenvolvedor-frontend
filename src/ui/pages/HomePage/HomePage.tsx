2f9658dc1bd8c77116ed67425f2bf56f9cc216e7
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-19 12:55:37 -0300

    Add Layout, SearchBar, LeafletCollection and Pagination in HomePage

--- src/ui/pages/HomePage/HomePage.tsx
+++ src/ui/pages/HomePage/HomePage.tsx
@@ -1,9 +1,30 @@
+import { useEffect } from "react";
+import { LeafletCollection, Pagination, SearchBar } from "../../components";
 import { Layout } from "../../components/Layout";
+import { useSearchContext } from "../../../context/search-context";
 
 export const HomePage = () => {
+  const {
+    leafletCollection,
+    searchLeafletPagination,
+    leafletCollectionPagination,
+  } = useSearchContext();
+
+  useEffect(() => {
+    if (!leafletCollectionPagination) {
+      searchLeafletPagination(1, false, "");
+    }
+  }, [searchLeafletPagination, leafletCollectionPagination]);
+
   return (
     <Layout>
-      <div>Search</div>
+      <SearchBar />
+      {leafletCollection.length && (
+        <LeafletCollection leafletList={leafletCollection} />
+      )}
+      {leafletCollectionPagination && (
+        <Pagination items={leafletCollectionPagination} />
+      )}
     </Layout>
   );
 };
