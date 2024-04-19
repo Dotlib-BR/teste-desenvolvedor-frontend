fe101ee17f11824b3b9d80a32b908c83e6b81223
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-19 15:09:51 -0300

    Refactor no result Pagination behavior

--- src/ui/pages/HomePage/HomePage.tsx
+++ src/ui/pages/HomePage/HomePage.tsx
@@ -23,7 +23,7 @@ export const HomePage = () => {
         title="Resultados"
         leafletCollection={leafletCollection}
       />
-      {leafletCollectionPagination && (
+      {leafletCollectionPagination?.data.length && (
         <Pagination items={leafletCollectionPagination} />
       )}
     </Layout>
