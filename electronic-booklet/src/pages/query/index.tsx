199243724cc881a4fe945ea6077949ad8682008f
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-19 11:30:37 -0300

    refactor: add loading component and refactored the title to show dynamic message

--- electronic-booklet/src/pages/query/index.tsx
+++ electronic-booklet/src/pages/query/index.tsx
@@ -1,33 +1,39 @@
 import { useLocation } from 'react-router-dom'
 import { useApiContext } from '../../context/api-provider'
+import { Items } from '../../interface/items'
 
 import MedicineDetails from '../../components/medicine-details'
+import Loading from '../../components/loading'
 
 import s from './style.module.sass'
-import { SelectedMedicineData } from '../../interface/selected-medicine-prop'
 
 export default function Query() {
-    const { medicine } = useApiContext()
+    const { items } = useApiContext()
     const location = useLocation()
     const search = new URLSearchParams(location.search).get('search')
 
-    const filteredData = medicine.filter(item => item.name === search)
-
-    if (filteredData.length === 0) {
-        return (
-            <div className={s.container}>
-                Nenhum resultado encontrado para "{search}"
-            </div>
-        )
+    if (!items) {
+        return <Loading />
     }
+
+    const filteredData = items.filter((item: Items) => item.name === search)
+
+    const dataExist =
+        filteredData.length === 0
+            ? `Nenhum resultado encontrado para "${search}"`
+            : `Resultados para: ${search}`
 
     return (
         <main className={s.container}>
-            <h1 className={s.titleSearch}>Resultados para: {search}</h1>
+            <h1 className={s.titleSearch}>{dataExist}</h1>
 
             <section className={s.searchItems}>
                 {filteredData.map(item => (
-                    <MedicineDetails key={item.id} data={item} />
+                    <MedicineDetails
+                        className={'className'}
+                        key={item.id}
+                        data={item}
+                    />
                 ))}
             </section>
         </main>
