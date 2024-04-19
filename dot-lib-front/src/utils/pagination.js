d40d60645ccb60af844538cdf36d2f07cf89fe43
MatheusMachad00 <matheus_m_machado@hotmail.com>
2024-04-19 15:13:44 -0300

    Fix: negative pages bug and blank pages bug fixed

--- dot-lib-front/src/utils/pagination.js
+++ dot-lib-front/src/utils/pagination.js
@@ -1,20 +1,44 @@
-export default function pagination(dataNewToOld, dataOLdToNew, searchbarResult, currentPage, itemsPerPage, setCurrentPage) {
+export default function pagination(
+  dataNewToOld,
+  dataOLdToNew,
+  searchbarResult,
+  currentPage,
+  itemsPerPage,
+  setCurrentPage
+) {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
   const paginatedDataNewToOld = dataNewToOld.slice(startIndex, endIndex);
   const paginatedDataOLdToNew = dataOLdToNew.slice(startIndex, endIndex);
   let paginatedDataSearchResult;
 
-  if (paginatedDataOLdToNew){
+  if (paginatedDataOLdToNew) {
     paginatedDataSearchResult = searchbarResult.slice(startIndex, endIndex);
   }
 
   const handlePreviousPage = () => {
-    setCurrentPage((prevPage) => prevPage - 1);
+    if (currentPage > 1) {
+      setCurrentPage((prevPage) => prevPage - 1);
+    }
   };
 
   const handleNextPage = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
+    const totalNumberOfPagesNewToOld = Math.ceil(dataNewToOld.length / itemsPerPage);
+    const totalNumberOfPagesOldToNew = Math.ceil(dataOLdToNew.length / itemsPerPage);
+    const totalNumberOfPagesSearchResult = Math.ceil(searchbarResult.length / itemsPerPage);
+  
+    let totalNumberOfPages;
+    if (paginatedDataNewToOld) {
+      totalNumberOfPages = totalNumberOfPagesNewToOld;
+    } else if (paginatedDataOLdToNew) {
+      totalNumberOfPages = totalNumberOfPagesOldToNew;
+    } else {
+      totalNumberOfPages = totalNumberOfPagesSearchResult;
+    }
+  
+    if (currentPage < totalNumberOfPages) {
+      setCurrentPage((prevPage) => prevPage + 1);
+    }
   };
 
   return {
@@ -22,6 +46,6 @@ export default function pagination(dataNewToOld, dataOLdToNew, searchbarResult, 
     paginatedDataOLdToNew,
     paginatedDataSearchResult,
     handlePreviousPage,
-    handleNextPage
+    handleNextPage,
   };
 }
