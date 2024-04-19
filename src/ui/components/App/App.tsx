930826225eb329572c7c061d3307dfa4955906e9
JPSS14 <jp_scarabelli@hotmail.com>
2024-04-19 12:56:35 -0300

    Add routes in App

--- src/ui/components/App/App.tsx
+++ src/ui/components/App/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "../../pages/";
+import { FavoritePage } from "../../pages/FavoritePage/FavoritePage";
 
 export const App = () => {
+  const basename = "/";
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter basename={basename}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
