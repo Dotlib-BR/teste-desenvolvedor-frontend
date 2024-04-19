8fdc95277e26a607f4156a6ea88cbe31aa87bd2f
Bruno Vinícius Barreiras de Oliveira <94939630+brunowzz@users.noreply.github.com>
2024-04-19 13:06:55 -0300

    feat: add not found page

--- electronic-booklet/src/pages/not-found/index.tsx
+++ electronic-booklet/src/pages/not-found/index.tsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router-dom'
+
+import s from './style.module.sass'
+
 export default function NotFound() {
-    return <main>notfound</main>
+    return (
+        <main className={s.container}>
+            <h1 className={s.title}>Página não encontrada...</h1>
+            <Link to='/'>
+                <button className={s.goHome}>Voltar para Home</button>
+            </Link>
+        </main>
+    )
 }
