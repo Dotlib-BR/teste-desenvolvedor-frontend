e71484500d1b504e4e3ba2a5507085d2da9004a7
Rodrigo Tissianel <rodrigo.tissianel@outlook.com>
2024-04-21 00:53:46 -0300

    style: adjust the spacing fo the card element

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -32,41 +32,45 @@ export const Card = ({
 
     return (
         <div className="card mt-2 mx-2">
-            <div className="flex-center">
-                <PillSVG />
+            <div>
+                <div className="flex-center">
+                    <PillSVG />
+                </div>
+
+                <h6 className="mt-3  line-clamp">
+                    <span className="weight-bold">Nome: </span>
+                    <span title={name}>{name}</span>
+                </h6>
+
+                <h6 className="my-1">
+                    <span className="weight-bold">Laboratório: </span>
+                    <span title={company}>{company}</span>
+                </h6>
+
+                <h6>
+                    <span className="weight-bold">Data de publicação: </span>
+                    {formattedDate}
+                </h6>
             </div>
 
-            <h6 className="mt-3  line-clamp">
-                <span className="weight-bold">Nome: </span>
-                <span title={name}>{name}</span>
-            </h6>
-
-            <h6 className="my-1">
-                <span className="weight-bold">Laboratório: </span>
-                <span title={company}>{company}</span>
-            </h6>
-
-            <h6>
-                <span className="weight-bold">Data de publicação: </span>
-                {formattedDate}
-            </h6>
-
-            <a
-                href={getDocument('PATIENT')}
-                target="_blank"
-                download={getDocument('PATIENT')}
-                className="download-btn mt-3"
-            >
-                Bula do paciente
-            </a>
-            <a
-                href={getDocument('PROFESSIONAL')}
-                target="_blank"
-                download
-                className="download-btn mt-2"
-            >
-                Bula do profissional
-            </a>
+            <div>
+                <a
+                    href={getDocument('PATIENT')}
+                    target="_blank"
+                    download={getDocument('PATIENT')}
+                    className="download-btn mt-3"
+                >
+                    Bula do paciente
+                </a>
+                <a
+                    href={getDocument('PROFESSIONAL')}
+                    target="_blank"
+                    download
+                    className="download-btn mt-2"
+                >
+                    Bula do profissional
+                </a>
+            </div>
         </div>
     );
 };
