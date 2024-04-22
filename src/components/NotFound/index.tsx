6c9d3f92ca34a78303cf40ac3fb123117d265635
Samuel Alves <samueltheodoro21@gmail.com>
2024-04-21 21:04:44 -0300

    error handler adicionado quando não há resposta do servidor

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -4,16 +4,33 @@ import { Container } from "./Styles"
 import notFoundIcon from '../../img/not-found.png'
 import { getAllMedications } from "../../functions/fetch_medications"
 import { DEFAULT_PAGE } from "../../utils/pagination"
+import { LoaderContext, LoaderContextType } from "../../contexts/Loader"
 
 export const NotFound = () => {
 
   const { MedicationState: { medicationData }, MedicationDispatch } = useContext(MedicationContext) as MedicationContextType
+  const { setLoading } = useContext(LoaderContext) as LoaderContextType 
 
   const fetchData = () => {
+    setLoading(true)
     getAllMedications(DEFAULT_PAGE)
     .then((data) => {
+
+      if (data.error) {
+        MedicationDispatch({
+          type: 'SET_ERROR',
+          payload: {
+            error: data.error,
+            errorMsg: data.errorMsg
+          }
+        })
+        setLoading(false)
+        return
+      }
+
       MedicationDispatch({ type: "SET_MEDICATION_DATA", payload: data })
       MedicationDispatch({ type: "SET_VALUE_SEARCHED", payload: { method: "none", value: '' } })
+      setLoading(false)
     })
   }
 
